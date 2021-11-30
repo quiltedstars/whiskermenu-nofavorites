28ae00bf6c5d1624c0453ae2ca826594bc473691
quiltedstars <87580563+quiltedstars@users.noreply.github.com>
2021-11-29 22:39:30 -0400

    Delete build directory

--- build/po/CMakeFiles/update_pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/update_pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/update_pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
