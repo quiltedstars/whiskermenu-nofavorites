2ebe5aed20b803106886411362f7230ec73340c6
quiltedstars <87580563+quiltedstars@users.noreply.github.com>
2021-11-29 22:37:15 -0400

    Add files via upload

---
+++ debug/panel-plugin/CMakeFiles/whiskermenu.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/register-plugin.c" "panel-plugin/CMakeFiles/whiskermenu.dir/register-plugin.c.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/register-plugin.c.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/applications-page.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/applications-page.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/applications-page.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/category-button.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/category-button.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/category-button.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/category.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/category.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/category.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/command-edit.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/command-edit.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/command-edit.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/command.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/command.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/command.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/element.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/element.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/element.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/favorites-page.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/favorites-page.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/favorites-page.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/icon-renderer.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/icon-renderer.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/icon-renderer.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/icon-size.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/icon-size.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/icon-size.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/launcher-icon-view.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/launcher-icon-view.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/launcher-icon-view.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/launcher-tree-view.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/launcher-tree-view.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/launcher-tree-view.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/launcher.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/launcher.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/launcher.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/page.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/page.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/page.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/plugin.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/plugin.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/plugin.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/profile.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/profile.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/profile.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/query.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/query.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/query.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/recent-page.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/recent-page.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/recent-page.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/resizer.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/resizer.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/resizer.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/run-action.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/run-action.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/run-action.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/search-action.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/search-action.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/search-action.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/search-page.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/search-page.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/search-page.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/settings-dialog.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/settings-dialog.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/settings-dialog.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/settings.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/settings.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/settings.cpp.o.d"
+  "/home/void/xfce4-whiskermenu-plugin/panel-plugin/window.cpp" "panel-plugin/CMakeFiles/whiskermenu.dir/window.cpp.o" "gcc" "panel-plugin/CMakeFiles/whiskermenu.dir/window.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
